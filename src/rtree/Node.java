f06e4fc53a397877380e66aab522a3cc26e2b222
dario617 <spam.dario@gmail.com>
2019-05-01 16:59:25 -0400

    Added memory manager v1

--- src/rtree/Node.java
+++ src/rtree/Node.java
@@ -3,15 +3,15 @@ package rtree;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Node implements Serializable{
+public class Node implements Serializable {
 	public boolean isLeaf;
-	public float[][] coords; 
+	public float[][] coords;
 	public ArrayList<Long> childs;
 	public ArrayList<float[][]> childDims;
 	public long parent;
 	public long myId;
-	
-	public Node(boolean isLeaf,float[][] coords, long parent) {
+
+	public Node(boolean isLeaf, float[][] coords, long parent) {
 		this.isLeaf = isLeaf;
 		this.coords = coords;
 		this.childs = new ArrayList<Long>();
