0290191de3b44bc81259612966bcf1f14753a96a
Darío Cáceres <dignaciocaceres@gmail.com>
2019-05-01 13:56:19 -0400

    added method for MBR calc

--- src/util/nRectangle.java
+++ src/util/nRectangle.java
@@ -1,12 +1,19 @@
 package util;
 
+import java.util.ArrayList;
+
 public class nRectangle {
 	float sum_epsilon;
 	float prod_epsilon;
+	static float min_value;
+	static float max_value;
+	
 	private nRectangle() {
 		super();
 		sum_epsilon = 0.001f;
 		prod_epsilon = 1.001f;
+		min_value = -1.0f * (float) Float.MAX_VALUE;
+		max_value = (float) Float.MAX_VALUE;
 	}
 	
 	public static boolean overlaps(float[][] nRect1, float[][] nRect2) {
@@ -16,18 +23,41 @@ public class nRectangle {
 			float upper_bound = Math.max(nRect1[i][0], nRect1[i][0] + nRect1[i][1]);
 			float lower_toCompare = Math.min(nRect2[i][0], nRect2[i][0] + nRect2[i][1]);
 			float upper_toCompare = Math.max(nRect2[i][0], nRect2[i][0] + nRect2[i][1]);
+			
 			overlaps = lower_bound <= lower_toCompare || lower_bound <= upper_toCompare
 					|| upper_bound >= lower_toCompare || upper_bound >= upper_toCompare;
+					
 			if(overlaps) break;
 		}
 		return overlaps;
 	}
 	
 	public static float[][] MBR(float[][]... nRects){
-		for(float[][] nRect : nRects) {
-			
+		float max_coord = min_value;
+		float min_coord = max_value;
+		
+		//generate initial MBR with full length sides
+		int mbr_len = nRects[0][0].length;
+		float[][] coords = new float[mbr_len][2];
+		for(int i = 0; i<mbr_len; i++) {
+			coords[i][0] = min_coord;
+			coords[i][1] = max_coord;
 		}
-		return null;
+		
+		for(float[][] nRect : nRects) {
+			for(int i = 0; i<nRect[0].length; i++) {
+				float lower_bound = Math.min(nRect[i][0], nRect[i][0] + nRect[i][1]);
+				float upper_bound = Math.max(nRect[i][0], nRect[i][0] + nRect[i][1]);
+				coords[i][0] = Math.min(lower_bound, coords[i][0]);				
+				coords[i][1] = Math.max(upper_bound, coords[i][1]);
+				
+			}
+		}
+		//transform into [coord, dim] system:
+		for(int i = 0; i<coords[0].length; i++) {
+			coords[i][1] -= coords[i][0];
+		}
+		return coords;
 	}
 	
 	public static float area(float[][] nRect) {
