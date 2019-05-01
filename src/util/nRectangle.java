d19a4ee4c1835e3a43404a819eed367f9f511721
Darío Cáceres <dignaciocaceres@gmail.com>
2019-05-01 14:45:59 -0400

    added checkEnlargement and modified parameters of functions

--- src/util/nRectangle.java
+++ src/util/nRectangle.java
@@ -3,8 +3,8 @@ package util;
 import java.util.ArrayList;
 
 public class nRectangle {
-	float sum_epsilon;
-	float prod_epsilon;
+	static float sum_epsilon;
+	static float prod_epsilon;
 	static float min_value;
 	static float max_value;
 	
@@ -16,6 +16,10 @@ public class nRectangle {
 		max_value = (float) Float.MAX_VALUE;
 	}
 	
+	/*
+	 * overlaps: Returns true if two rectangles overlap, false otherwise.
+	 * 
+	 * */
 	public static boolean overlaps(float[][] nRect1, float[][] nRect2) {
 		boolean overlaps = false;
 		for(int i = 0; i<nRect1[0].length; i++) {
@@ -32,12 +36,23 @@ public class nRectangle {
 		return overlaps;
 	}
 	
-	public static float[][] MBR(float[][]... nRects){
+	/*
+	 * returns the Minimum Bounding (n)Rectangle of a set of (n)Rectangles as a float array.
+	 * 
+	 */
+	
+	public static float[][] MBR(float[][] nRect1, float[][] nRect2){
+		ArrayList<float[][]> twoRects = new ArrayList();
+		twoRects.add(nRect1);
+		twoRects.add(nRect2);
+		return MBR(twoRects);
+	}
+	public static float[][] MBR(ArrayList<float[][]> nRects){
 		float max_coord = min_value;
 		float min_coord = max_value;
 		
 		//generate initial MBR with full length sides
-		int mbr_len = nRects[0][0].length;
+		int mbr_len = nRects.get(0)[0].length;
 		float[][] coords = new float[mbr_len][2];
 		for(int i = 0; i<mbr_len; i++) {
 			coords[i][0] = min_coord;
@@ -60,6 +75,9 @@ public class nRectangle {
 		return coords;
 	}
 	
+	/*
+	 * returns the area of a rectangle.
+	 */
 	public static float area(float[][] nRect) {
 		float accumulator = 1;
 		for(int i=0; i<nRect[1].length; i++) {
@@ -68,8 +86,36 @@ public class nRectangle {
 		return accumulator;
 	}
 	
-	public static float checkEnlargement(){
-		return 0;
+	/* 
+	 * Return the index of the best rectangle based in enlargement.
+	 * 
+	 */
+	public static int checkEnlargement(float[][] newRect, ArrayList<float[][]> nRects){
+		int actual_index = 0;
+		float best_area = (float) Float.MAX_VALUE; //for tie checking.
+		float best_enlargement = (float) Float.MAX_VALUE;
+		int saved_index = 0;
+		for(float[][] nRect : nRects) {
+			float[][] mbr = MBR(newRect, nRect);
+			float enlargement = area(mbr)-area(nRect);
+			if(Math.abs(enlargement - best_enlargement) < sum_epsilon ) { //enlargements son "iguales"
+				//Check which area is lower.
+				if(area(nRect)<best_area) {
+					saved_index = actual_index;
+					best_enlargement = enlargement;
+				} else {
+					//the other rectangle is the best rectangle.
+				}
+			} else if(enlargement<best_enlargement) {
+				saved_index = actual_index;
+				best_enlargement = enlargement;
+			} else {
+				//Rectangle is not good, do nothing.
+			}
+			actual_index++;
+		}		
+		
+		return saved_index;
 	}
 	
 }
