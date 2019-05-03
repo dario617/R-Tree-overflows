66f5ff527f030a5acb90710a97effbb508d06d5b
sebastiancif <sebastian.cifuentes@ing.uchile.cl>
2019-05-03 17:10:53 -0400

    cosas

---
+++ src/test/nRectangleTest.java
@@ -0,0 +1,38 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class nRectangleTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void overlaps() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void MBR() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void MBR1() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void area() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void checkEnlargement() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void enlargementArea() {
+    }
+}
