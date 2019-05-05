3547a863f729ec7fe7fa42b1a972ab5ff806f77f
sebastiancif <sebastian.cifuentes@ing.uchile.cl>
2019-05-05 14:41:21 -0400

    separados test quadratic y linear

--- src/test/RTreeTest.java
+++ src/test/RTreeTest.java
@@ -9,7 +9,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RTreeTest {
 
-    RTree theTree;
+    RTree qTree;
+    RTree lTree;
     private float[][] r1 = {{1,0},{1,1}};
     private float[][] r2 = {{4,1},{3,2}};
     private float[][] r3 = {{8,2},{3,1}};
@@ -23,44 +24,75 @@ class RTreeTest {
 
     @BeforeEach
     void setUp() {
-        theTree = new RTree(1,3, 2, RTree.OverflowMethod.LINEAR, 10);
-        theTree.insertRect(r1);
-        theTree.insertRect(r2);
-        theTree.insertRect(r3);
+        qTree = new RTree(1,3, 2, RTree.OverflowMethod.QUADRATIC, 10);
+        qTree.insertRect(r1);
+        qTree.insertRect(r2);
+        qTree.insertRect(r3);
+        lTree = new RTree(1,3, 2, RTree.OverflowMethod.LINEAR, 10);
+        lTree.insertRect(r1);
+        lTree.insertRect(r2);
+        lTree.insertRect(r3);
     }
 
     @AfterEach
     void tearDown() {
-        theTree = null;
+        qTree = null;
+        lTree = null;
     }
 
     @Test
-    void insertRect() {
-        assertEquals(3, theTree.rootSize());
-        theTree.insertRect(r4);
-        assertEquals(2, theTree.rootSize());
-        theTree.insertRect(r5);
-        theTree.insertRect(r6);
-        theTree.insertRect(r7);
-        assertEquals(3, theTree.rootSize());
+    void insertRectLinear() {
+        assertEquals(3, lTree.rootSize());
+        lTree.insertRect(r4);
+        assertEquals(2, lTree.rootSize());
+        lTree.insertRect(r5);
+        lTree.insertRect(r6);
+        lTree.insertRect(r7);
+        assertEquals(3, lTree.rootSize());
     }
 
     @Test
-    void search() {
-        assertTrue(theTree.search(r1));
-        assertTrue(theTree.search(r2));
-        assertTrue(theTree.search(r3));
-        assertFalse(theTree.search(r4));
-        theTree.insertRect(r4);
-        theTree.insertRect(r5);
-        theTree.insertRect(r6);
-        assertTrue(theTree.search(r1));
+    void searchLinear() {
+        assertTrue(lTree.search(r1));
+        assertTrue(lTree.search(r2));
+        assertTrue(lTree.search(r3));
+        assertFalse(lTree.search(r4));
+        lTree.insertRect(r4);
+        lTree.insertRect(r5);
+        lTree.insertRect(r6);
+        assertTrue(lTree.search(r1));
         System.out.println("SEARCH R4 CON 6 NODOS");
-        assertTrue(theTree.search(r4));
+        assertTrue(lTree.search(r4));
         System.out.println("SEARCH R6 CON 6 NODOS");
-        assertTrue(theTree.search(r6));
-        assertFalse(theTree.search(r7));
+        assertTrue(lTree.search(r6));
+        assertFalse(lTree.search(r7));
+    }
 
+    @Test
+    void insertRectQuadratic() {
+        assertEquals(3, qTree.rootSize());
+        qTree.insertRect(r4);
+        assertEquals(2, qTree.rootSize());
+        qTree.insertRect(r5);
+        qTree.insertRect(r6);
+        qTree.insertRect(r7);
+        assertEquals(3, qTree.rootSize());
+    }
 
+    @Test
+    void searchQuadratic() {
+        assertTrue(qTree.search(r1));
+        assertTrue(qTree.search(r2));
+        assertTrue(qTree.search(r3));
+        assertFalse(qTree.search(r4));
+        qTree.insertRect(r4);
+        qTree.insertRect(r5);
+        qTree.insertRect(r6);
+        assertTrue(qTree.search(r1));
+        System.out.println("SEARCH R4 CON 6 NODOS");
+        assertTrue(qTree.search(r4));
+        System.out.println("SEARCH R6 CON 6 NODOS");
+        assertTrue(qTree.search(r6));
+        assertFalse(qTree.search(r7));
     }
 }
